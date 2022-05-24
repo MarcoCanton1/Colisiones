73861e6cd913671885a776bf4367924e1bd20c41
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-05-24 14:28:50 -0300

    Ayo de pizza here
    aaaaaaaaaaaaaaa

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidadMov;
+    public float velocidadRot;
+
+    public bool isable;
+    public GameObject huevo;
+
+
+    void Start()
+    {
+        isable = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, velocidadMov);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -velocidadMov);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, velocidadRot, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles += new Vector3(0, -velocidadRot, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(huevo);
+            clon.transform.position = transform.position - new Vector3(0, 0, -1);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "EnablerCube"){
+            isable = true;
+        }
+        else if (col.gameObject.name == "DisablerCube")
+        {
+            isable = false;
+        }
+    }
+}
---
+++ Assets/colision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colision : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter()
+    {
+        //hace contacto
+    }
+    void OnCollisionExit()
+    {
+        //deja de hacer contacto
+    }
+    void OnCollisionStay()
+    {
+        //mientras este haciendo contacto (constante)
+    }
+}
